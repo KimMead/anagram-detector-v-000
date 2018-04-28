9b57c363596e6e1b913fe140a9fbea99cff1e584
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-28 23:18:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,14 @@
 class Anagram
   attr_accessor :word
-@@all = []
 
 def initialize(word)
   @word = word
 end
 
-def self.all
-  @@all
-end
-
-def anagram_match
-
-end
+def match(find_anagram_in_string)
+  find_anagram_in_string.find_all do |word|
+    if word.split("").sort == self.word.split("").sort
+      word
+    end
+  end
 end
